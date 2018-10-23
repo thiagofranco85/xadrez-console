683883b202995cbb233497555b05c04056c3e690
thiagofranco85 <thiago.boo@gmail.com>
2018-10-23 21:49:34 -0200

    Inicio do projeto xadrez-console

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3,4);
+
+            Console.WriteLine(P);
+        }
+    }
+}
